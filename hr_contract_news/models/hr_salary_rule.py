c30ce7462d2e8d67281714ff323fe21e0e17cffa
Jose Ernesto Mendez <jose.ernesto.mendez@gmail.com>
2020-06-09 08:21:03 -0400

    Initial commit

--- hr_contract_news/models/hr_salary_rule.py
+++ hr_contract_news/models/hr_salary_rule.py
@@ -82,3 +82,24 @@ class HrSalaryRule(models.Model):
                 lambda b: b.hr_contract_news_concepts_id in self.employee_news_ids)
 
         return news
+
+
+    @api.multi
+    def compute_rule(self, localdict):
+        self.ensure_one()
+
+        # Add reference to the rule itself
+        localdict['rule'] = self
+
+        # The payslip contained in the local dict is an object that is
+        # different from an actual payslip. The real payslip is
+        # contained in the attribute 'dict' of that object.
+        payslip = localdict['payslip']
+        if not isinstance(payslip, type(self.env['hr.payslip'])):
+            payslip = payslip.dict
+            payslip.refresh()
+            localdict['payslip'] = payslip
+
+        # Pass the rule_id parameter, because the parent function
+        # has a not standard signature
+        return super(HrSalaryRule, self).compute_rule(self.id, localdict)
